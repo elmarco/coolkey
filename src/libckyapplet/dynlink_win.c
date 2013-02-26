009a95c00be8eb3a3ae0616c5f76acb727a85c88
Marc-André Lureau <marcandre.lureau@gmail.com>
2013-02-26 14:26:56 +0100

    win32: include windows.h no Windows.h

--- src/libckyapplet/dynlink_win.c
+++ src/libckyapplet/dynlink_win.c
@@ -17,7 +17,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  * ***** END COPYRIGHT BLOCK ***** */
 
-#include <Windows.h>
+#include <windows.h>
 #include "cky_base.h"
 #include "dynlink.h"
 
